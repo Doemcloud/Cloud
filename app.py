1fab08838c2222339442e624e7f0be08e13d9dd3
Doemcloud <thundermiho@gmail.com>
2024-10-08 22:02:07 +0700

    +

--- app.py
+++ app.py
@@ -234,6 +234,7 @@ def share_file(file_id):
 
 # Запуск сервера
 if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
     with app.app_context():
         db.create_all()  # создание таблиц в базе данных
         if not os.path.exists(app.config['UPLOAD_FOLDER']):
